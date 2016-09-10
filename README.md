eb637b7dfaedace7b3e7aecd4d9eb72fb14fb1e8
David Barnett <david@barnett.net.nz>
2016-09-11 10:53:55 +1200

    remove multirust completion
    add nvim plugins to update
    update readme

--- README.md
+++ README.md
@@ -2,13 +2,8 @@
 
 Collection of zsh scripts and completions
 
-## install
+## Install
 
 Using [zgen](https://github.com/tarjoilija/zgen)
 
 Add `zgen load zyphrus/zsh-scripts` to your `.zshrc`
-
-## What is included?
-
-* [multirust](https://github.com/brson/multirust) completions (under
-  contruction)
--- util.zsh
+++ util.zsh
@@ -3,6 +3,10 @@ function update-config {
     ( cd ~/.dotfiles && git pull )
     echo "Updating neovim config"
     ( cd ~/.config/nvim && git pull )
+    echo "Installing neovim plugins"
+    ( nvim -c PlugInstall -c :qa ) 
+    echo "Updating neovim plugins"
+    ( nvim -c PlugUpdate -c :qa ) 
     echo "Updating zsh config"
     zgen update
 }
